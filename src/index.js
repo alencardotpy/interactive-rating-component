5ac6d07e38eeb5311e04c47b68c34bfb3c70d22f
Carlos Alencar <68407281+alencardotpy@users.noreply.github.com>
2022-06-26 23:53:45 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
 import { sendToVercelAnalytics } from "./vitals";
 import { BrowserRouter } from "react-router-dom";
+import AttributionContainer from "./Attribution/Attribution";
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
+      <AttributionContainer />
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById("root")
