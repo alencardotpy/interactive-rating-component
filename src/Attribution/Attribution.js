31ebaff335607112fe3abbef6de88a2064398312
Carlos Alencar <68407281+alencardotpy@users.noreply.github.com>
2022-06-26 23:53:42 -0300

    Create Attribution.js

---
+++ src/Attribution/Attribution.js
@@ -0,0 +1,23 @@
+import "./Attribution.css";
+
+function AttributionContainer() {
+  return (
+    <div className="attribution">
+      <p className="attribution__text">
+        Challenge by
+        <a
+          className="attribution__link"
+          href="https://www.frontendmentor.io?ref=challenge"
+        >
+          Frontend Mentor
+        </a>
+        Coded by
+        <a className="attribution__link" href="https://github.com/alencardotpy">
+          alencarDotPy.
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default AttributionContainer;
